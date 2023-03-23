d912ec562297e1f0833cafaf6fc30fb28486a4ff
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-23 16:02:49 +0100

    add slider

--- assets/main.js
+++ assets/main.js
@@ -23,3 +23,7 @@ const images = [
 ];
 
 console.log(images);
+
+/*
+1 - Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
+*/
